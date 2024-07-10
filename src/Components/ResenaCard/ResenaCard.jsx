60d9910da0756057e63ce83e7f2d15938e0f004a
Daniel Echeverri <echeverri121@gmail.com>
2024-07-09 20:35:45 -0500

    Añadir fecha visualizada en las reseñas

--- src/Components/ResenaCard/ResenaCard.jsx
+++ src/Components/ResenaCard/ResenaCard.jsx
@@ -3,6 +3,18 @@ import './ResenaCard.css';
 
 function ResenaCard({ resena, user, deleteResena, setNuevaRes }) {
     const firebaseUserId = localStorage.getItem('firebaseId');
+    const dateString = resena.fechaResena;
+  
+    // Convert the string to a Date object
+    const dateObj = new Date(dateString);
+    
+    // Extract day, month, and year from the Date object
+    const day = dateObj.getDate();
+    const month = dateObj.getMonth() + 1; // getMonth() returns zero-based month, so add 1
+    const year = dateObj.getFullYear();
+    
+    // Construct formatted date string
+    const formattedDate = `${day}-${month}-${year}`;
 
     const handleDelete = () => {
         deleteResena(resena.resenaId);
@@ -11,7 +23,7 @@ function ResenaCard({ resena, user, deleteResena, setNuevaRes }) {
     if(resena.usuario.username === user && firebaseUserId === resena.usuarioId){
         return (
             <div className="resena-card">
-              <h3>{resena.usuario.username}.</h3>
+              <h3>{resena.usuario.username}.</h3><p>{formattedDate}</p>
               <div id="borde">
                 <p id="comentario">{resena.comentario}</p>
               </div>      
@@ -26,7 +38,7 @@ function ResenaCard({ resena, user, deleteResena, setNuevaRes }) {
     else {
         return (
             <div className="resena-card">
-              <h3>{resena.usuario.username}</h3>
+              <h3>{resena.usuario.username}</h3><p>{formattedDate}</p>
               <div>
                 <p id="comentario">{resena.comentario}</p>
               </div>      
